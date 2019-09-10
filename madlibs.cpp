7498a985ba642600d1d1216dff1f54b4aa09241f
adgilbert@mail.csuchico.edu <adgilbert@ecc-jaguar.csuchico.edu>
2019-09-09 17:47:48 -0700

    we learn how to code a madlips

--- madlibs.cpp
+++ madlibs.cpp
@@ -1,9 +1,31 @@
-//Authors: 
+//Authors:Andrew Gilbert
 #include <iostream>
 #include <string>
 
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+
 int main()
 {
+    string name="";
+    string adverb="";
+    string noun="";
+    int x;
+    int y;
+
+    cout<<"enter a first name";
+    cin>>name;
+    cout<<"enter a noun";
+    cin>>adverb;
+    cout<<"enter a noun";
+    cin>>noun;
+    cout<<"enter a number";
+    cin>>x;
+    cout<<"enter a number";
+    cin>>y;
+    cout<<"As Garfield & "<<name <<"took in the devestation of the world of ruin around them wordlessly took hold of each others hands. Nothing will everbe th... I'm "<<adverb <<"on my own. Will you be my "<<noun <<"I need you he asked sadly gazing deeply into each others eyes. Its been "<<x <<"days since the world ended & "<<y <<"years since I've felt something."; 
 
   return 0;
 }
